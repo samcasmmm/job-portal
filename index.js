9b7254115baa6f6a5dd782251c23e53aaf470b63
Sameer <sameerbagwan132@gmail.com>
2023-08-11 21:09:17 +0530

    added database

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-//
-
-// imports
+//packages imports
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import cors from 'cors';
+
+//file imports
+import connectDB from './config/db.js';
 
 // REST Object
 
 const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
 
 // routes
 
@@ -20,8 +26,17 @@ app.get('/', (req, res) => {
 
 // listen
 
-app.listen(process.env.PORT, () => {
-  console.log(
-    `${process.env.NODE_ENV} Server on :- http://localhost:${process.env.PORT}`
-  );
-});
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+  } catch (error) {
+    console.log(error);
+  }
+  app.listen(process.env.PORT, () => {
+    console.log(
+      `${process.env.NODE_ENV} server on http://localhost:${process.env.PORT}`
+    );
+  });
+};
+
+startServer();
